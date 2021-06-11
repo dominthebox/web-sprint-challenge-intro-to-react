e993a177226e89a5bcfb48c53a4e864e74ad29ba
dominthebox <dsallustro@gmail.com>
2021-06-11 13:35:04 -0400

    wrote my component, and rendered data on the page, started some styling and will continue to play with styling a bit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import { BASE_URL } from "./components/SWData";
+import styled from "styled-components";
+import Character from "./components/Character";
+
+const StyledHead = styled.h1`
+  color:darkgray;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +18,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}`)
+    .then(res => {
+      setCharacters(res.data);
+      console.log(res.data);
+    })
+    .catch(error => {
+      console.log('Houston, we have a problem', error);
+    })
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <StyledHead className="Header">Characters</StyledHead>
+      {characters.map(character => {
+        return <Character key={character.index} characters={character} />
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,49 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+    display: flex;
+    align-self: auto;
+    justify-content: space-evenly;
+`
+
+const StyledName = styled.h2`
+    color: blue;
+    display: flex;
+    align-items: center;
+    background-color: tan;
+    border-radius: 15px;
+`
+
+const StyledInfo = styled.h4`
+    color: dodgerblue;
+    background-color: tan;
+    border-radius: 15px;
+`
+
+export default function Character({ characters }) {
+    const {name, height, mass, eye_color, gender, birth_year} = characters;
+    
+    return (
+        <StyledDiv className="char-info">
+            <StyledName>
+                Name: {characters.name}
+            </StyledName>
+            <StyledInfo>
+                <span className="char-details">
+                    Birth year: {birth_year}
+                    <br></br>
+                    Gender: {gender}
+                    <br></br>
+                    Height: {height}
+                    <br></br>
+                    Mass: {mass}
+                    <br></br>
+                    Eyes: {eye_color}
+                    <br></br>
+                </span>
+            </StyledInfo>
+        </StyledDiv>
+    )
+}
